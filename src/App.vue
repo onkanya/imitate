cc36ff5cef334756f82e20f19c65c220ecbaa367
onkanya <onkanyalearning@gmail.com>
2018-12-06 17:59:53 +0700

    update1

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
-  <v-container>
-    <v-layout align-center justify-center row>
-        Hi there, I'm Stash
+<div>
+  <v-container mb-5>
+    <v-layout row wrap >
+      <v-flex text-md-center text-xs-center class="mb-3">
+        <v-card flat>
+          <v-card-text>
+            Hi there, I'm Stash
+          </v-card-text>
+        </v-card>
+      </v-flex>
     </v-layout>
-    <br>
 
-    <v-layout align-center justify-center row>
-      <img src="/img/profile.jpg" class="profile"></img>
+    <v-layout align-center justify-center row class="mb-3">
+      <img src="/img/profile.jpg" class="profile">
     </v-layout>
-    <br>
 
-    <v-layout align-center justify-center row>
+    <v-layout align-center justify-center row class="mb-4">
           <p class="text-md-center">
             I'm a Freelance Interactive Art Director based in France. Focusing across branding and identiry, digital and print.
           </p>
     </v-layout>
-    <br>
 
-    <v-layout align-center justify-center row>
-      <v-toolbar>
+    <v-layout row wrap class="mb-5">
+      <v-flex md10 offset-md1>
+      <v-toolbar class="elevation-0" style="border-bottom: 2px solid; border-top: 0.2px solid;background-color: white;">
         <v-toolbar-title>ogato</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
@@ -31,114 +36,225 @@
           <v-toolbar-side-icon></v-toolbar-side-icon>
         </v-toolbar-items>
       </v-toolbar>
+      </v-flex>
     </v-layout>
-    <br>
+
+    <v-layout row wrap>
+        <!-- left -->
+          <v-flex md7 offset-md1 mr-2>
+            <img src="/img/cafe.jpg" class="img-content">
+            <v-layout row pb-2>
+              <v-flex md10 offset-md1>
+                <v-card class="card--flex-toolbar">
+                  <v-toolbar card prominent >
+                    <img src="/img/profile.jpg" class="profile-content">
+                    <v-toolbar-title class="caption grey--text">
+                        by : orgato __ 
+                        <font color="red">life</font> 
+                        __ February 27,2018
+                    </v-toolbar-title>
+
+                    <v-spacer></v-spacer>
+
+                    <v-btn icon>
+                      <v-icon>search</v-icon>
+                    </v-btn>
+
+                    <v-btn icon>
+                      <v-icon>apps</v-icon>
+                    </v-btn>
+
+                    <v-btn icon>
+                      <v-icon>more_vert</v-icon>
+                    </v-btn>
+                  </v-toolbar>
+
+                  <v-divider></v-divider>
+
+                  <v-card-text>
+                      <h2 style="margin-bottom: 15px;">Makeup Primers for Everyday Wear</h2>
+                      
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at. 
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+
+            <img src="/img/cafe.jpg" class="img-content">
+            <v-layout row pb-2>
+              <v-flex md10 offset-md1>
+                <v-card class="card--flex-toolbar">
+                  <v-toolbar card prominent >
+                    <img src="/img/profile.jpg" class="profile-content">
+                    <v-toolbar-title class="caption grey--text">
+                        by : orgato __ 
+                        <font color="red">life</font> 
+                        __ February 27,2018
+                    </v-toolbar-title>
+
+                    <v-spacer></v-spacer>
+
+                    <v-btn icon>
+                      <v-icon>search</v-icon>
+                    </v-btn>
+
+                    <v-btn icon>
+                      <v-icon>apps</v-icon>
+                    </v-btn>
+
+                    <v-btn icon>
+                      <v-icon>more_vert</v-icon>
+                    </v-btn>
+                  </v-toolbar>
+
+                  <v-divider></v-divider>
+
+                  <v-card-text>
+                      <h2 style="margin-bottom: 15px;">Makeup Primers for Everyday Wear</h2>
+                      
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at. 
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+    
 
     <!-- right -->
-    <v-layout>
-      <v-flex md9>
-        <div class="parent">
-          <img src="/img/cafe.jpg" alt="" class="img-content">
-          <div class="child">
+          <v-flex md3>
+            <div style="text-align: center;">
+              <p>About Me</p>
+              <img src="/img/profile.jpg" alt="" class="profile"> 
+              <p>
+                Schilitz knausgad occupy master clnse normcore health goth. Coloring book i street, art umami pok biosel brunch cloud bread.
+              </p>
+              <v-spacer>Recent Posts</v-spacer> 
 
-          </div>
-        </div>
-        
-      </v-flex>
+              <v-list>
+                <v-list-tile>
+                  <v-list-tile-avatar>
+                    <img src="/img/list1.jpg"  alt="">
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                    <v-list-tile-sub-title>
+                      Hairstyle
+                    </v-list-tile-sub-title>
+                    <v-list-tile-title>
+                      6 Hairstyle Ideas for Instant Refresh Look
+                    </v-list-tile-title>               
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+              <v-list>
+                <v-list-tile>
+                  <v-list-tile-avatar>
+                    <img src="/img/list2.jpg"  alt="">
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                    <v-list-tile-sub-title>
+                      Hairstyle
+                    </v-list-tile-sub-title>
+                    <v-list-tile-title>
+                      6 Hairstyle Ideas for Instant Refresh Look
+                    </v-list-tile-title>               
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+              <v-list>
+                <v-list-tile>
+                  <v-list-tile-avatar>
+                    <img src="/img/list3.jpg"  alt="">
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                    <v-list-tile-sub-title>
+                      Hairstyle
+                    </v-list-tile-sub-title>
+                    <v-list-tile-title>
+                      6 Hairstyle Ideas for Instant Refresh Look
+                    </v-list-tile-title>               
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+              <v-spacer>Follow Me</v-spacer>
+              <v-spacer>Instagram</v-spacer>
 
-      <v-flex md3>
-        <div style="text-align: center;">
-          <p>About Me</p>
-          <img src="/img/profile.jpg" alt="" class="profile"> 
-          <p>
-            Schilitz knausgad occupy master clnse normcore health goth. Coloring book i street, art umami pok biosel brunch cloud bread.
-          </p>
-          <v-spacer>Recent Posts</v-spacer> 
+              <v-container fluid grid-list-xs>
+              <v-layout row wrap>
+                <v-flex
+                  v-for="i in 4"
+                  :key="i"
+                  xs6
+                >
+                  <img
+                    :src="`https://randomuser.me/api/portraits/women/${i + 5}.jpg`"
+                    alt="lorem"
+                    class="image"
+                    height="100%"
+                    width="100%"
+                  >
+                </v-flex>
+              </v-layout>
+            </v-container>
 
-          <v-list>
-            <v-list-tile>
-              <v-list-tile-avatar>
-                <img src="/img/list1.jpg"  alt="">
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-sub-title>
-                  Hairstyle
-                </v-list-tile-sub-title>
-                <v-list-tile-title>
-                  6 Hairstyle Ideas for Instant Refresh Look
-                </v-list-tile-title>               
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-          <v-list>
-            <v-list-tile>
-              <v-list-tile-avatar>
-                <img src="/img/list2.jpg"  alt="">
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-sub-title>
-                  Hairstyle
-                </v-list-tile-sub-title>
-                <v-list-tile-title>
-                  6 Hairstyle Ideas for Instant Refresh Look
-                </v-list-tile-title>               
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-          <v-list>
-            <v-list-tile>
-              <v-list-tile-avatar>
-                <img src="/img/list3.jpg"  alt="">
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-sub-title>
-                  Hairstyle
-                </v-list-tile-sub-title>
-                <v-list-tile-title>
-                  6 Hairstyle Ideas for Instant Refresh Look
-                </v-list-tile-title>               
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-          <v-spacer>Follow Me</v-spacer>
-          <v-spacer>Instagram</v-spacer>
-
-          <v-container fluid grid-list-xs>
-          <v-layout row wrap>
-            <v-flex
-              v-for="i in 4"
-              :key="i"
-              xs6
-            >
-              <img
-                :src="`https://randomuser.me/api/portraits/women/${i + 5}.jpg`"
-                alt="lorem"
-                class="image"
-                height="100%"
-                width="100%"
+            <v-spacer>Subscribe</v-spacer>
+            
+            <v-flex>
+              <v-text-field
+                label="Your Email Address"
+                solo
+              ></v-text-field>
+              <v-btn
+                class="btn-subscribe"
               >
+                SIGN IN
+              </v-btn>
             </v-flex>
-          </v-layout>
-        </v-container>
-
-        <v-spacer>Subscribe</v-spacer>
-        
-        <v-flex xs12 sm12 md12>
-          <v-text-field
-            label="Solo"
-            solo
-          ></v-text-field>
-        </v-flex>
-        </div>
-      </v-flex>
+            </div>
+            
+          </v-flex>
     </v-layout>
-
-    <div class="parent">
+    <!-- <div class="parent">
       <div class="child">
-
+        <v-layout row wrap>
+          <v-flex>
+            <img src="/img/profile.jpg" alt="">
+          </v-flex>
+        </v-layout>
       </div>
-    </div>
+    </div> -->
   </v-container>
+
+  <v-footer mt-5>
+    <v-card
+      flat
+      tile
+      width="100%"
+      class="black white--text text-xs-center"
+    >
+      <v-card-text>
+        <v-icon name="facebook"></v-icon>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        SIGN UP FOR OUR WEEKLY NEWSLETTER
+      </v-card-text>
+
+      <v-flex xs4 offset-xs4>
+        <v-text-field
+          label="Your Email Address"
+          solo
+        ></v-text-field>
+      </v-flex>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        &copy;2018 — Design Grid. All Rights Reserved.
+      </v-card-text>
+    </v-card>
+  </v-footer>
+
+</div>
 </template>
 
 <script>
@@ -158,9 +274,24 @@
     height: 100px;
     border-radius: 100px;
   }
+  
+  .profile-content {
+    width: 50px;
+    height: 50px;
+    border-radius: 50px;
+  }
+
+  .card--flex-toolbar {
+    margin-top: -50px;
+  }
+
+  .btn-subscribe {
+    color: white;
+    background-color: black;
+  }
 
   .img-content {
-    width: 600px;
+    width: 100%;
   }
 
   .parent {
@@ -178,5 +309,8 @@
     }
   }
 
+  .icon {
+    width: 50px;
+  }
 </style>
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
+import feather from 'vue-icon'
 
 Vue.use(Vuetify)
+Vue.use(feather, 'v-icon')
 
 new Vue({
   el: '#app',
